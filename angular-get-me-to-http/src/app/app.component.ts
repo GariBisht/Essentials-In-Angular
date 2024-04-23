71a99310cfff3dbb153bee9647ee15ab3175526d
Garima Bisht <74089873+GariBisht@users.noreply.github.com>
2024-04-23 08:51:30 +0530

    http file uploaded
    http

---
+++ angular-get-me-to-http/src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+//Used to show data
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+//  users=[1 ,2 ,3]
+  users: any;
+  url = "https://api.github.com/users";
+  isLoading = false;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.isLoading = true;
+    this.http.get(this.url).subscribe(res => {
+      console.log('success');
+      console.log(res);
+      setTimeout(() => {
+        this.isLoading = false;
+        this.users = res;
+      }, 3000);
+    }, error => {
+      this.isLoading = true;
+      console.log('error');
+      console.log(error);
+    }, () => {
+      console.log('completed');
+    });
+  }
+
+  goToGithub(url) {
+    window.open(url, '_blank');
+  }
+}
